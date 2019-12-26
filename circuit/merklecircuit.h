c0909cddcb970b2c8ccdb6e45b112a0aa9a4661d
Star.LI <star@trapdoortech.com>
2019-12-26 20:38:28 +0800

    Remove a wrong assert statement
    Signed-off-by: Star.LI <star@trapdoortech.com>

--- circuit/merklecircuit.h
+++ circuit/merklecircuit.h
@@ -32,7 +32,6 @@ namespace sample {
             leaf_digest= std::make_shared<digest_variable<FieldT>>(pb, digest_size, "leaf");
             path_var = std::make_shared<merkle_authentication_path_variable<FieldT, HashT>>(pb, tree_depth, "path");
             address_bits_var.allocate(pb, tree_depth, "address_bits");
-	    assert(address_bits_var.get_field_element_from_bits(pb).as_ulong() == address);
             merkle = std::make_shared<merkle_tree_check_read_gadget<FieldT, HashT>>(pb, tree_depth, address_bits_var, *leaf_digest, *root_digest, *path_var, ONE, "merkle");
             pb.set_input_sizes(root_digest->digest_size);
         }
